1d6dbae1e322ad60151929312bcf9adc0f42a80d
Mitsuharu Emoto <mthr1982@gmail.com>
2022-01-23 18:24:43 +0900

    fix

--- app/android/app/src/main/java/com/ubepedia/MainActivity.java
+++ app/android/app/src/main/java/com/ubepedia/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ubepedia;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +12,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ubepedia";
+  }
+
+
+  /**
+   * https://reactnavigation.org/docs/getting-started/
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
