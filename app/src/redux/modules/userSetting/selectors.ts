e0c69f4e4f96fef122adaedf93b22b510ea0b70b
Mitsuharu Emoto <mthr1982@gmail.com>
2022-02-12 19:12:41 +0900

    rename

--- app/src/redux/modules/userSetting/selectors.ts
+++ app/src/redux/modules/userSetting/selectors.ts
@@ -1,4 +1,4 @@
 import { RootState } from '@/redux/RootState'
 
-export const selectOnLongPressCopyToClipboard = (state: RootState) =>
-  state.userSetting.onLongPressCopyToClipboard
+export const selectCanCopyToClipboardOnLongPress = (state: RootState) =>
+  state.userSetting.canCopyToClipboardOnLongPress
