6fa43126e8aa4622e9072b2be80bed1ba679be55
Mitsuharu Emoto <mthr1982@gmail.com>
2022-02-15 11:52:59 +0900

    fix theme

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -3,17 +3,14 @@ import React from 'react'
 import { MainParams } from './main.params'
 import { RootRoutes } from './root.routes'
 import { Provider as PaperProvider } from 'react-native-paper'
-import { useNavigationTheme } from './theme/useNavigationTheme'
-import { usePaperTheme } from './theme/usePaperTheme'
+import { useAppTheme } from './theme/useAppTheme'
 
 /**
  * @see https://reactnavigation.org/docs/auth-flow/
  * @see https://callstack.github.io/react-native-paper/getting-started.html
  */
 const Routes: React.FC = () => {
-  const paperTheme = usePaperTheme()
-  const navigationTheme = useNavigationTheme()
-
+  const { paperTheme, navigationTheme } = useAppTheme()
   return (
     <PaperProvider theme={paperTheme}>
       <NavigationContainer theme={navigationTheme}>
